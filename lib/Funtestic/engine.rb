fea218f47f743a1caca2f965dd87f70a489c643e
Tom Amel <ameltom@gmail.com>
2015-08-04 11:30:54 +0300

    ActionView::Base include instead of ActionController::Base helper

--- lib/Funtestic/engine.rb
+++ lib/Funtestic/engine.rb
@@ -2,7 +2,7 @@ module Funtestic
   class Engine < ::Rails::Engine
     initializer "funtestic" do |app|
       ActionController::Base.send :include, Funtestic::Helper
-      ActionController::Base.helper Funtestic::Helper
+      ActionView::Base.send :include, Funtestic::Helper
     end
   end
 end
